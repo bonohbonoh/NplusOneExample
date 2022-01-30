3aa34e045295ba82051353efbe31b92547c898c5
bonohbonoh <psmb2006@naver.com>
2022-01-30 23:11:50 +0900

    Initial commit

---
+++ src/main/java/com/example/nplusoneexample/NplusOneExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.nplusoneexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NplusOneExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NplusOneExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nplusoneexample/NplusOneExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nplusoneexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NplusOneExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
